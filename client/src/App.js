cd1cd349d62073d6b12404f4fa1cba8fae597485
Victor Turitzin <turitzinv@gmail.com>
2023-01-28 11:51:59 -0500

    Test push. Removed previous code to test set up.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((resp) => resp.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((resp) => resp.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
     <BrowserRouter>
     <div className="App">
       <Switch>
+        <Route exact path = "/" >
+          <h1>Test Home</h1>
+        </Route>
         <Route path="/testing">
           <h1>Test Route</h1>
-        </Route>
-        <Route path="/">
-          <h1>Page Count: {count}</h1>
         </Route>
       </Switch>
     </div>
