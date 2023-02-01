1b13b5d4b716070d1824f6f6356c6dc1a04fe6c5
Victor Turitzin <turitzinv@gmail.com>
2023-01-31 20:26:09 -0500

    Corrected Fallback controller to say ActionController::Base. Added get users route to test migrations and seeded data.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
 
   get '/hello', to: 'application#hello_world'
 
-  get '*path',
-      to: 'fallback#index',
-      constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get '*path',
+  #     to: 'fallback#index',
+  #     constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  get '/users', to: 'users#index'
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.create(username: "VictorT", password: "VictorT123")
 
 List.create(
   header: "Packing List",
-  user_id: 1,
+  user_id: 1
 )
 
 Item.create(point: "Toothbrush", list_id: 1)
