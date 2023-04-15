e62a9c6d3c987d1de987fc7aeb4b6da03c5fd799
Victor Turitzin <turitzinv@gmail.com>
2023-04-15 14:14:29 -0400

    Built mock version of MyLists component.

--- client/src/components/MyLists.js
+++ client/src/components/MyLists.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const MyLists = () => {
   return (
-    <div>MyLists Component</div>
-  )
-}
+    <div>
+      <h1>MyLists</h1>
+      <button>Add New List</button>
+      <h2>Grocery List</h2>
+      <h2>Packing List</h2>
+      <h2>Other List</h2>
+    </div>
+  );
+};
 
-export default MyLists
+export default MyLists;
