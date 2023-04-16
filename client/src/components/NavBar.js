9c11b9b7ac0511e7aa7487c2b6d60f7f56d86049
Victor Turitzin <turitzinv@gmail.com>
2023-04-16 15:10:31 -0400

    Added css to NavBar.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,12 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+
+  const linkStyles = {
+    display: "inline-block",
+    width: "140px",
+    padding: "5px 5px",
+    margin: "5px 5px 5px",
+    background: "rgb(30, 124, 105)",
+    textDecoration: "none",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "20px"
+  }
+
   return (
     <div>
-      <NavLink to="/">
+      <NavLink style={linkStyles} to="/">
         Home
       </NavLink>
-      <NavLink to="/lists">
+      <NavLink style={linkStyles} to="/lists">
         My Lists
       </NavLink>
     </div>
