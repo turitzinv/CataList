e3a7cc2b7d458956c45f9ce4019581bd8029a203
Victor Turitzin <turitzinv@gmail.com>
2023-04-29 13:22:26 -0400

    Experimenting with NavBar css.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,27 +3,27 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
 
-  const linkStyles = {
-    display: "inline-block",
-    width: "140px",
-    padding: "5px 5px",
-    margin: "5px 5px 5px",
-    background: "rgb(30, 124, 105)",
-    textDecoration: "none",
-    color: "white",
-    fontWeight: "bold",
-    fontSize: "20px"
-  }
+  // const linkStyles = {
+  //   display: "inline-block",
+  //   width: "140px",
+  //   padding: "5px",
+  //   margin: "5px",
+  //   background: "rgb(30, 124, 105)",
+  //   textDecoration: "none",
+  //   color: "white",
+  //   fontWeight: "bold",
+  //   fontSize: "20px"
+  // }
 
   return (
     <div>
-      <NavLink style={linkStyles} to="/">
+      <NavLink class="navbar" to="/">
         Home
       </NavLink>
-      <NavLink style={linkStyles} to="/lists">
+      <NavLink class="navbar" to="/lists">
         My Lists
       </NavLink>
-      <NavLink style={linkStyles} to="/login">
+      <NavLink class="navbar" to="/login">
         Login
       </NavLink>
     </div>
