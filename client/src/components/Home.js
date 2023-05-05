3db217dba831403451d59dc96e35fe19312ee0f6
Victor Turitzin <turitzinv@gmail.com>
2023-05-05 14:19:32 -0400

    Updated class to className in Home component.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 class="home-headers">Full-Lists</h1>
-      <h2 class="home-headers">Your lists at its fullest</h2>
+      <h1 className="home-headers">Full-Lists</h1>
+      <h2 className="home-headers">Your lists at its fullest</h2>
       <p>Store your lists for just about anything! Packing, shopping, goals, agendas, list them all!</p>
     </div>
   );
