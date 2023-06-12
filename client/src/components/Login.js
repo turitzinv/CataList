55d290635384cb2e4a423f66c8a01f8eab43ae89
Victor Turitzin <turitzinv@gmail.com>
2023-06-12 12:19:59 -0400

    Within Login component, corrected class to className.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,14 +6,14 @@ const Login = ({handleLogin, setUsername, setPassword, username, password}) => {
       <h2>Please log into your Full-List account</h2>
       <form id="login-form" onClick={handleLogin}>
         <input 
-        class="login-input"
+        className="login-input"
         type= "text"
         placeholder= "Username"
         onChange={(e) => setUsername(e.target.value)}
         value={username}
         />
         <input 
-        class="login-input"
+        className="login-input"
         type= "password"
         placeholder= "Password"
         onChange={(e) => setPassword(e.target.value)}
